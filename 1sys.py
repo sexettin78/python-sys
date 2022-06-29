ca40442aa31408a05ae9035cafebdba8a7b3965c
Sexettin <72075007+sexettin78@users.noreply.github.com>
2022-06-29 05:24:51 +0300

    Add files via upload

---
+++ 1sys.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.argv #programımız çalıştırılırken verilen parametreleri bir liste halinde tutar
+
+sys.exit() #programdan çıkış yapmamızı sağlar
+
+sys.executable #python arayüzünün dizin yolu verilir
+
+sys.getwindowsversion() #yalnızca windows işletim sisteminde çalışır. sistem hakkında bilgi verir
+
+sys.path #python modüllerinin bulunduğu yolları verir
+
+sys.platform #işletim sistemi hakkında bilgi verir
+
+sys.prefix #pythonun kurulu olduğu yeri gösterir
+
+#sys.ps1 = "f >" #yaparsak pythonda çalıştırdığımız kodlar >>> ön eki yerine f > ön eki ile çalışır
+
+sys.version #python sürümünü verir
+
+sys.version_info #python sürümünün ayrıntılı bilgilerini verir. major: büyük sürüm, minor: küçük sürüm, micro: en küçük sürüm.
+
+sys.stdout #pythonun varsayılan çıktı verme işlemini gösterebiliriz
+
+sys.stderr #python programlama dilinin varsayılan hata verme işlemini görebiliriz
+
+sys.stdin #python programlama dilinin kullanıcıdan varsayılan bilgi alma işlemini görebiliriz
+
